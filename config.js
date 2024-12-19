2edc36cc7112d33ec294f6678350c56c170bf0c2
Pemitha Ranmia <98648094+PPEMITHARANMIRA@users.noreply.github.com>
2024-12-19 22:39:12 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "", //put your session id
+SESSION_ID: process.env.SESSION_ID || "gpp1GT5C#JNVe3M3Kk6ybsx0tzf-EYTxHsHjL7uZRsj1fWonC9W8", 
 ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/2nmi9q.png",
 MONGODB: process.env.MONGODB || "mongodb+srv://sam:sam@cluster0.u1smxsv.mongodb.net/?retryWrites=true&w=majority",
 AUTO_VOICE:"true" //true or false
